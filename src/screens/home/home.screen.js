2b140ff4d373f39ea030a5cd3e43412b1354bdd7
GuillaumeF93 <frognier.guillaume@gmail.com>
2023-07-05 21:10:15 +0200

    Update Temporary HomeScreen to include ForgotPasswordScreen LoginScreen RegisterScreen

--- src/screens/home/home.screen.js
+++ src/screens/home/home.screen.js
@@ -3,8 +3,17 @@ import { Button, SafeAreaView, View } from "react-native";
 export const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView>
-      <Button title="error" onPress={() => navigation.navigate("Error")} />
-      <Button title="contact" onPress={() => navigation.navigate("Contact")} />
+      <Button title="Error" onPress={() => navigation.navigate("Error")} />
+      <Button title="Contact" onPress={() => navigation.navigate("Contact")} />
+      <Button
+        title="Forgot Password"
+        onPress={() => navigation.navigate("ForgotPassword")}
+      />
+      <Button title="Login" onPress={() => navigation.navigate("Login")} />
+      <Button
+        title="Register"
+        onPress={() => navigation.navigate("Register")}
+      />
     </SafeAreaView>
   );
 };
