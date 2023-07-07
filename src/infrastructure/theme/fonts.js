3bf0a834bf920e9a0f6ab7dc436651cc88dc1a6b
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-07-07 09:19:59 +0200

    feat : added home page and H1

--- src/infrastructure/theme/fonts.js
+++ src/infrastructure/theme/fonts.js
@@ -1,11 +1,12 @@
 export const fonts = {
   body: "Alata_400Regular",
-  heading: "Lora_400Regular",
+  heading: "Lora_600SemiBold",
 };
 
 export const fontWeights = {
   regular: 400,
   medium: 500,
+  semiBold: 600,
   bold: 700,
 };
 
---
+++ src/screens/NavBar/navBar.screen.js
@@ -0,0 +1,23 @@
+import { Button, SafeAreaView, View } from "react-native";
+import { H1 } from "../../../src/components/theme";
+
+export const NavBar = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <H1>Bonjour</H1>
+
+      <Button title="Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Error" onPress={() => navigation.navigate("Error")} />
+      <Button title="Contact" onPress={() => navigation.navigate("Contact")} />
+      <Button
+        title="Forgot Password"
+        onPress={() => navigation.navigate("ForgotPassword")}
+      />
+      <Button title="Login" onPress={() => navigation.navigate("Login")} />
+      <Button
+        title="Register"
+        onPress={() => navigation.navigate("Register")}
+      />
+    </SafeAreaView>
+  );
+};
