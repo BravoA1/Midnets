b2226224bab6f0e8754db1c8edb45a3694e458a2
michaelvb-24 <michael.vanbouchotte1@gmail.com>
2023-07-07 09:56:35 +0200

    Update style in ContactScreen

--- src/screens/contact/contact.screen.js
+++ src/screens/contact/contact.screen.js
@@ -2,56 +2,62 @@ import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { styled } from "styled-components";
 import Logo from "../../components/Logo";
 
-const Paragraph1 = styled.Text`
+const P = styled.Text`
   font-family: ${(props) => props.theme.fonts.body};
+`;
+
+const Titre = styled.Text`
+  font-family: ${(props) => props.theme.fonts.heading};
 `;
 
 export const ContactScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Logo height={"default"}/>
-      <Paragraph1>
-        Lorem ipsum dolor sit amet consectetur. Dolores asperiores eum ducimus
-        culpa saepe! Soluta totam aut repellendus illo vero. Quam optio
-        consequatur sunt mollitia praesentium. Laborum ut earum aut ducimus
-        molestias. Dolor obcaecati pariatur ab quae minus? Nisi non labore
-        pariatur assumenda dolores! Architecto, repudiandae aut? Dolorum,
-        inventore recusandae.
-      </Paragraph1>
-      <View style={styles.phoneContainer}>
-        <Image
-          source={require("../../img/appel.png")}
-          style={styles.phoneImg}
-        />
-        <Text style={styles.phoneNumber}>583 123 456</Text>
-      </View>
-      <View style={styles.emailContainer}>
-        <Image
-          source={require("../../img/arobase.png")}
-          style={styles.arobaseImg}
-        />
-        <Text style={styles.email}>midnets@outlook.fr</Text>
-      </View>
-      <View style={styles.socialNetworkContainer}>
-        <Image
-          style={styles.network}
-          source={require("../../img/facebook.png")}
-        />
-        <Image
-          style={styles.network}
-          source={require("../../img/instagram.png")}
-        />
-        <Image
-          style={styles.network}
-          source={require("../../img/twitter.png")}
-        />
-      </View>
-      <View style={styles.creditContainer}>
-        <Text style={styles.creditTitle}>Crédits</Text>
-        <Text style={styles.creditDescription}>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Logo height={"default"} />
+        <P style={styles.p1}>
           Lorem ipsum dolor sit amet consectetur. Dolores asperiores eum ducimus
-          culpa saepe! Soluta totam aut repellendus illo vero.
-        </Text>
+          culpa saepe! Soluta totam aut repellendus illo vero. Quam optio
+          consequatur sunt mollitia praesentium. Laborum ut earum aut ducimus
+          molestias. Dolor obcaecati pariatur ab quae minus? Nisi non labore
+          pariatur assumenda dolores! Architecto, repudiandae aut? Dolorum,
+          inventore recusandae.
+        </P>
+        <View style={styles.phoneContainer}>
+          <Image
+            source={require("../../img/appel.png")}
+            style={styles.phoneImg}
+          />
+          <Titre style={styles.phoneNumber}>583 123 456</Titre>
+        </View>
+        <View style={styles.emailContainer}>
+          <Image
+            source={require("../../img/arobase.png")}
+            style={styles.arobaseImg}
+          />
+          <Titre style={styles.email}>midnets@outlook.fr</Titre>
+        </View>
+        <View style={styles.socialNetworkContainer}>
+          <Image
+            style={styles.network}
+            source={require("../../img/facebook.png")}
+          />
+          <Image
+            style={styles.network}
+            source={require("../../img/instagram.png")}
+          />
+          <Image
+            style={styles.network}
+            source={require("../../img/twitter.png")}
+          />
+        </View>
+        <View style={styles.creditContainer}>
+          <Titre style={styles.creditTitle}>Crédits</Titre>
+          <P>
+            Lorem ipsum dolor sit amet consectetur. Dolores asperiores eum
+            ducimus culpa saepe! Soluta totam aut repellendus illo vero.
+          </P>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -60,71 +66,70 @@ export const ContactScreen = () => {
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height: "auto",
-    flex: 1,
+    height: "100%",
     backgroundColor: "#fff",
     alignItems: "center",
-    // alignContent: 'center',
     justifyContent: "center",
-    padding: "15%",
+    padding: "10%",
   },
   p1: {
-    marginTop: "8%",
+    marginTop: "5%",
+    lineHeight: "18%",
   },
   phoneContainer: {
-    margin: 0,
+    // margin: 0,
     width: "100%",
+    marginTop: "5%",
     height: "10%",
     flex: 1,
     flexDirection: "row",
-    // marginTop: '10%',
-    justifyContent: "center",
+    justifyContent: "space-beetween",
     alignItems: "center",
   },
   phoneImg: {
     resizeMode: "contain",
     width: "25%",
-    height: "25%",
+    height: "45%",
   },
   phoneNumber: {
     fontSize: 32,
   },
   emailContainer: {
     margin: 0,
+    marginBottom: "10%",
+    marginTop: "5%",
     width: "100%",
     height: "10%",
     flex: 1,
     flexDirection: "row",
-    justifyContent: "center",
-    // alignItems: 'center',
+    justifyContent: "space-beetween",
+    alignItems: "center",
   },
   arobaseImg: {
     resizeMode: "contain",
     width: "25%",
-    height: "25%",
+    height: "45%",
   },
   email: {
     fontSize: 20,
-    // marginLeft: '5%',
   },
   socialNetworkContainer: {
-    margin: 0,
     width: "100%",
     height: "10%",
+    marginBottom: "5%",
     flex: 1,
     flexDirection: "row",
-    justifyContent: "center",
-    // alignItems: 'center',
+    justifyContent: "space-around",
   },
   network: {
     resizeMode: "contain",
-    width: "35%",
-    height: "35%",
+    width: "45%",
+    height: "55%",
   },
   creditContainer: {
     margin: 0,
     width: "100%",
-    height: "10%",
+    height: "30%",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
@@ -132,6 +137,6 @@ const styles = StyleSheet.create({
   },
   creditTitle: {
     fontSize: 32,
-    marginBottom: "5%",
+    marginBottom: "2%",
   },
 });
