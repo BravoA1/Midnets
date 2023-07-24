a028c2cf4b119c64a706c2d2cd85b1e92591ff6d
michaelvb-24 <michael.vanbouchotte1@gmail.com>
2023-07-24 15:42:09 +0200

    design quizz finish

--- src/screens/quizz/quizz.screen.js
+++ src/screens/quizz/quizz.screen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, SafeAreaView } from "react-native";
 import React from "react";
 import { styled } from "styled-components/native";
 import { LinearGradient } from "expo-linear-gradient";
@@ -51,12 +51,13 @@ const ResponseContainer2 = styled.View`
   display: flex;
   justify-content: space-evenly;
   flex-flow: row nowrap;
+  margin-bottom: ${(props) => props.theme.space[2]};
 `;
 const Response = styled.Text`
   font-family: ${(props) => props.theme.fonts.body};
 `;
 const MoreContainer = styled.View`
-  margin-top: ${(props) => props.theme.space[3]};
+  margin-top: ${(props) => props.theme.space[4]};
   width: 100%;
   display: flex;
   justify-content: space-evenly;
@@ -66,76 +67,92 @@ const MoreText = styled.Text`
   font-family: ${(props) => props.theme.fonts.headingBold};
 `;
 
-const ProgresBar = styled.View``;
-const ProgresPoint = styled.View``;
+const ProgresBar = styled.View`
+  width: 65%;
+  height: 2%;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+`;
+const ProgresPoint = styled.View`
+  width: 5%;
+  height: 100%;
+  background-color: gray;
+  border-radius: 20px;
+  margin-left: 2%;
+  margin-right: 2%;
+`;
 
 export const QuizzScreen = () => {
   return (
-    <Container>
-      <InsetShadow
-        containerStyle={styles.shadow}
-        shadowRadius={10}
-        shadowOpacity={20}
-        bottom={false}
-        left={false}
-      >
-        <Linear
-          colors={["#D8C2EF", "rgba(255,255,255,0)"]}
-          locations={[0, 1]}
-          start={{ x: 1, y: 0 }}
-          end={{ x: 0, y: 1 }}
+    <SafeAreaView>
+      <Container>
+        <InsetShadow
+          containerStyle={styles.shadow}
+          shadowRadius={10}
+          shadowOpacity={20}
+          bottom={false}
+          left={false}
         >
-          <TitleContainer style={styles.container}>
-            <Title>Quiz: les femmes</Title>
-          </TitleContainer>
-        </Linear>
-      </InsetShadow>
-      <View style={styles.imgContainer}>
-        <Image
-          source={require("../../img/quizzMarieCurie.jpg")}
-          style={styles.img}
-        />
-      </View>
-      {/* View qui contient la question */}
-      <QuestionContainer>
-        <Question>Qu'a découvert Marie Curie ?</Question>
-      </QuestionContainer>
-      {/* button reponse */}
-      <ResponseContainer1>
-        <ButtonResponse>
-          <Response>Le radium</Response>
-        </ButtonResponse>
-        <ButtonResponse>
-          <Response>Le cuivre</Response>
-        </ButtonResponse>
-      </ResponseContainer1>
-      <ResponseContainer2>
-        <ButtonResponse>
-          <Response>Le curry vert</Response>
-        </ButtonResponse>
-        <ButtonResponse>
-          <Response>La mimolette</Response>
-        </ButtonResponse>
-      </ResponseContainer2>
-      {/* barre de progression avec les petits */}
-      <ProgresBar>
-        <ProgresPoint></ProgresPoint>
-        <ProgresPoint></ProgresPoint>
-        <ProgresPoint></ProgresPoint>
-        <ProgresPoint></ProgresPoint>
-        <ProgresPoint></ProgresPoint>
-        <ProgresPoint></ProgresPoint>
-        <ProgresPoint></ProgresPoint>
-      </ProgresBar>
-      {/* button poour accèder aux règles */}
-      <ButtonRules></ButtonRules>
-      {/* en savoir plus container */}
-      <MoreContainer>
-        <ButtonResponse>
-          <MoreText>En savoir plus</MoreText>
-        </ButtonResponse>
-      </MoreContainer>
-    </Container>
+          <Linear
+            colors={["#D8C2EF", "rgba(255,255,255,0)"]}
+            locations={[0, 1]}
+            start={{ x: 1, y: 0 }}
+            end={{ x: 0, y: 1 }}
+          >
+            <TitleContainer style={styles.container}>
+              <Title>Quiz: les femmes</Title>
+            </TitleContainer>
+          </Linear>
+        </InsetShadow>
+        <View style={styles.imgContainer}>
+          <Image
+            source={require("../../img/quizzMarieCurie.jpg")}
+            style={styles.img}
+          />
+        </View>
+        {/* View qui contient la question */}
+        <QuestionContainer>
+          <Question>Qu'a découvert Marie Curie ?</Question>
+        </QuestionContainer>
+        {/* button reponse */}
+        <ResponseContainer1>
+          <ButtonResponse>
+            <Response>Le radium</Response>
+          </ButtonResponse>
+          <ButtonResponse>
+            <Response>Le cuivre</Response>
+          </ButtonResponse>
+        </ResponseContainer1>
+        <ResponseContainer2>
+          <ButtonResponse>
+            <Response>Le curry vert</Response>
+          </ButtonResponse>
+          <ButtonResponse>
+            <Response>La mimolette</Response>
+          </ButtonResponse>
+        </ResponseContainer2>
+        {/* barre de progression avec les petits */}
+        <ProgresBar>
+          <ProgresPoint></ProgresPoint>
+          <ProgresPoint></ProgresPoint>
+          <ProgresPoint></ProgresPoint>
+          <ProgresPoint></ProgresPoint>
+          <ProgresPoint></ProgresPoint>
+          <ProgresPoint></ProgresPoint>
+          <ProgresPoint></ProgresPoint>
+        </ProgresBar>
+        {/* button poour accèder aux règles */}
+        <ButtonRules></ButtonRules>
+        {/* en savoir plus container */}
+        <MoreContainer>
+          <ButtonResponse>
+            <MoreText>En savoir plus</MoreText>
+          </ButtonResponse>
+        </MoreContainer>
+      </Container>
+    </SafeAreaView>
   );
 };
 
